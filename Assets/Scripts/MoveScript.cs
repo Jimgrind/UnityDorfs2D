b319517842385de3e329ecd59d02723c2ee1eea9
Shadowizard <Neomaster2300@gmail.com>
2020-08-18 21:00:28 -0700

    Got movement to work, early tilemaps

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    Rigidbody2D playerRigid;
+    public float speed = 30;
+    float cancelDist = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerRigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //We will need to fix diagonal movement eventually
+        float hoInput = Input.GetAxisRaw("Horizontal");
+        float veInput = Input.GetAxisRaw("Vertical");
+
+        playerRigid.MovePosition(playerRigid.position + (new Vector2(hoInput, veInput).normalized * speed * Time.deltaTime) );
+
+        //Rotation
+        float y = Input.mousePosition.y - (Screen.height/2);
+        float x = Input.mousePosition.x - (Screen.width/2);
+        float angle = Mathf.Atan2(y,x);
+
+        if(new Vector2(x,y).magnitude > cancelDist){
+          playerRigid.MoveRotation((Mathf.Rad2Deg*angle)-90);
+        }
+
+    }
+}
