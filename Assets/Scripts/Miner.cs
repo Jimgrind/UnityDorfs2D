beedc3372cecdaafe05d3b04f3bf628fae63c64a
Benjamin Green <benjaminisaacgreen@gitlab.com>
2020-08-20 23:33:07 -0700

    Added broken features

---
+++ Assets/Scripts/Miner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Miner : MonoBehaviour
+{
+
+    public GameObject gridGameObject;
+    Rigidbody2D playerRigid;
+    public Camera cam;
+
+    Tilemap tilemap;
+    Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody2D>();
+        if (gridGameObject != null)
+        {
+            grid = gridGameObject.GetComponent<Grid>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+        //Vector3Int mp = new Vector3Int((int) Mathf.Floor(mouse.x), (int) Mathf.Floor(mouse.y), (int) Mathf.Ceil(mouse.z));
+        //if (Input.GetButton("Fire1") && tilemap != null)
+        //{
+        //    if(tilemap.GetTile(mp))
+         //   {
+        //        Debug.Log(mp);
+        //        if (tilemap.GetTile(mp).name == "Wal") 
+        //        {
+        //            tilemap.SetTile(tilemap.WorldToCell(mp), null);
+        //        }
+        //    } else
+        //    {
+         //       Debug.Log("No Tile Found at " + mp);
+        //    }
+        //}
+    }
+}
--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,28 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
 
 public class MoveScript : MonoBehaviour
 {
-    Rigidbody2D playerRigid;
+    Rigidbody playerRigid;
+    Tilemap curr_tilemap;
+    Tilemap up_tilemap;
+    Tilemap down_tilemap;
     public float speed = 30;
+    public GameObject gridGameObject;
+    public Camera cam;
     float cancelDist = 20;
+    Grid grid;
+
 
     // Start is called before the first frame update
     void Start()
     {
-      playerRigid = GetComponent<Rigidbody2D>();
-
+        playerRigid = GetComponent<Rigidbody>();
+        //Transform transform;
+        if (gridGameObject != null)
+        {
+            grid = gridGameObject.GetComponent<Grid>();
+        }
     }
 
+    void Stairs()
+    {
+        Vector3 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int mp = new Vector3Int((int) Mathf.Floor(mouse.x), (int) Mathf.Floor(mouse.y), (int) Mathf.Ceil(mouse.z));
+        if (Input.GetButton("Fire1") && curr_tilemap != null)
+        {
+            Debug.Log("Checking Stairs");
+            if(curr_tilemap.GetTile(mp))
+           {
+                Debug.Log(mp);
+                if (curr_tilemap.GetTile(mp).name == "Stairs")
+                {
+                    Debug.Log("TP");
+                    transform.TransformPoint(new Vector3(0,0,transform.position.z - 1));
+                }
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
-    {
+    {   
+        foreach(Tilemap tilemap in grid.GetComponentsInChildren<Tilemap>()) {
+            if(tilemap.origin.z == Mathf.Ceil(transform.position.z))
+            {
+                curr_tilemap = tilemap;
+                Debug.Log("Found Tilemap");
+                break;
+            }
+        }
+
+        Stairs();
         //We will need to fix diagonal movement eventually
         float hoInput = Input.GetAxisRaw("Horizontal");
         float veInput = Input.GetAxisRaw("Vertical");
 
-        playerRigid.MovePosition(playerRigid.position + (new Vector2(hoInput, veInput).normalized * speed * Time.deltaTime) );
+        playerRigid.MovePosition(playerRigid.position + (new Vector3(hoInput, veInput, 0).normalized * speed * Time.deltaTime) );
 
         //Rotation
         float y = Input.mousePosition.y - (Screen.height/2);
@@ -30,7 +71,7 @@ public class MoveScript : MonoBehaviour
         float angle = Mathf.Atan2(y,x);
 
         if(new Vector2(x,y).magnitude > cancelDist){
-          playerRigid.MoveRotation((Mathf.Rad2Deg*angle)-90);
+          //playerRigid.MoveRotation((Mathf.Rad2Deg*angle)-90);
         }
 
     }
