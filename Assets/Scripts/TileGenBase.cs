b7a85e463e9fe6f60efd985a652583adf4345878
Colton Jelsema <thecoltonjelsema@gmail.com>
2020-08-28 16:32:20 -0700

    The worst unity merge ever
    idk how any of the lib stuff is supposed to be working

---
+++ Assets/Scripts/TileGenBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class TileGenBase : MonoBehaviour
+{
+    protected Vector3Int size;
+
+    protected int border;
+
+    public GameObject gm_game_object;
+    private GridManager gm;
+    protected Tilemap tilemap;
+
+
+    protected void Init()
+    {
+        if(gm_game_object != null)
+        {
+            gm = gm_game_object.GetComponent<GridManager>();
+        } else
+        {
+            Debug.Log("No Grid Manager Found");
+        }
+        //size = gm.GetSize();
+        //border = gm.GetBorder();
+        //tilemap = GetComponent<Tilemap>();
+        
+        //Debug.Log("Size: " + size);
+        //Debug.Log("Border " + border);
+    }
+}
