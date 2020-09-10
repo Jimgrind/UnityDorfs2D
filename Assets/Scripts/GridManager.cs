9935a95a0a1101c3d7ba195838bb0740f4f6e8de
Shadowizard <Neomaster2300@gmail.com>
2020-09-09 17:07:08 -0700

    Broke and then fixed stairs. Also worldgen.

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,13 +8,35 @@ using UnityEngine.Tilemaps;
 public class GridManager : MonoBehaviour
 {
 
-    public int Offset = 64; // This should be static, but if it is it will break things
-    public int levels = 10;
-    private int Half;
+    //Side length of the square of each "world layer"
+    public int layerSize = 16;
+    //The thickness in tiles of the buffer between world layers
+    public int bufferSize = 6;
+    //Number of levels
+    public int numLayers = 10;
+
+    public int offset; // This should be static, but if it is it will break things
+
+    private int halfBuffer;
 
     // Terrain generation
     public Tilemap map;
     public Tile barrier;
+
+    //Creates a rectangle of barrier tiles from bottom left to top right
+    private void BarrierRect(int x1, int y1, int x2, int y2){
+
+      for(int i = x1;i < x2;i++){
+
+        for(int j = y1;j < y2;j++){
+
+          map.SetTile(new Vector3Int(i,j,0),barrier);
+
+        }
+      }
+    }
+
+    /*
 
     private void BuildWall(float offset)
     {
@@ -27,8 +49,8 @@ public class GridManager : MonoBehaviour
         }
     }
 
-    //Builds a right-aligned U shape of barrier
-    private void BuildBarrier(float offset)
+    //Horseshoes right-aligned
+    private void BuildBarrier()
     {
         BuildWall(offset+1);
         for (int x = (int) (Offset * (offset - 0.5) + 3); x < (int) (Offset * (offset + 0.5) - 3); ++x)
@@ -45,18 +67,35 @@ public class GridManager : MonoBehaviour
             }
         }
     }
+    */
 
     // Start is called before the first frame update.
     // Create borders
     void Start()
     {
-        Half = Offset / 2;
-    
-        BuildWall(0); //Cap it off
-        for (int i = 0; i < levels; ++i)
-        {
-            BuildBarrier(i);
-        }
+
+      offset = layerSize + bufferSize;
+
+      /*
+      halfBuffer = bufferSize/2;
+
+      int Xlength = numLayers*layerSize + (numLayers+1)*bufferSize;
+      BarrierRect(0,0,Xlength,bufferSize);
+      BarrierRect(0,offset-bufferSize,Xlength,offset);
+
+      for(int i = 0;i < numLayers+1;i++){
+        BarrierRect(i*offset-bufferSize,bufferSize,i*offset+bufferSize,offset-bufferSize);
+      }
+      */
+
+      int Xlength = numLayers*layerSize + (numLayers+1)*bufferSize;
+      BarrierRect(0,0,Xlength,bufferSize);
+      BarrierRect(0,offset,Xlength,offset+bufferSize);
+
+      for(int i = 0;i < numLayers+1;i++){
+        BarrierRect(i*offset,bufferSize,i*offset+bufferSize,offset);
+      }
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/WorldGen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WorldGen : TileGenBase
+{
+    public Tile wall;
+
+    private void Rect(Tile tile,int x1, int y1, int x2, int y2){
+
+      for(int i = x1;i < x2;i++){
+
+        for(int j = y1;j < y2;j++){
+
+          gm.map.SetTile(new Vector3Int(i,j,0),tile);
+
+        }
+      }
+    }
+
+    private void FillLayer(Tile tile,int layer){
+
+      Rect(tile,offset*layer+gm.bufferSize,gm.bufferSize,offset*(layer+1),offset);
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+
+        Init();
+
+        for(int i = 0;i < gm.numLayers;i++){
+          FillLayer(wall,i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+}
