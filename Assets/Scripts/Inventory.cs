c212ef4349909805fa62f04d557caa9e6776f691
Benjamin Green <benjaminisaacgreen@gitlab.com>
2020-09-10 12:56:29 -0700

    Added Stockpiles. Began build menu

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    List<StockPile> piles;
+
+    public GameObject inventory;
+    // Start is called before the first frame update
+    void Start() {
+        piles = new List<StockPile>();
+    }
+
+    public void AddStockPile(StockPile pile) {
+        Debug.Log("Adding Pile");
+        piles.Add(pile);
+    }
+
+    public void RemoveStockPile(StockPile pile) {
+        Debug.Log("Removing Pile");
+        piles.Remove(pile);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Something Happened");
+        if (other.gameObject.tag == "stockpile") {
+            Debug.Log("Collision with Player");
+            AddStockPile(other.gameObject.GetComponent<StockPile>());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("Something Happened");
+
+        if (other.gameObject.tag == "stockpile") {
+            Debug.Log("Player Left");
+            RemoveStockPile(other.gameObject.GetComponent<StockPile>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (piles != null) {
+            foreach (StockPile pile in piles) {
+                Debug.Log(pile.title);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/StockPile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockPile : MonoBehaviour
+{
+
+    public string title;
+    Dictionary<string, ItemStack> items;
+    public ItemStack.ItemType type;
+    Collider2D accessZone;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        items = new Dictionary<string, ItemStack>();
+    }
+
+    void Init(ItemStack.ItemType type) {
+        this.type = type;
+    }
+
+    bool AddItem(ItemStack item) {
+        if(item.type == this.type || this.type == ItemStack.ItemType.Main) {
+            if(items[item.name] != null) {
+                items[item.name].amount += item.amount;
+            } else {
+                items.Add(item.name, item);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool RemoveItem(ItemStack item, int amount) {
+        if(items[item.name] != null && items[item.name].amount >= amount) {
+            items[item.name].amount -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
