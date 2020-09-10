b116f6401ccb185ef25499001f911d5f71f5a553
Benjamin Green <benjaminisaacgreen@gitlab.com>
2020-09-10 13:24:34 -0700

    Added Build Menu Toggle

--- Assets/Scripts/BuildMenu.cs
+++ Assets/Scripts/BuildMenu.cs
@@ -9,15 +9,22 @@ public class BuildMenu : MonoBehaviour
     public GameObject stockpile;
     public GameObject stairs;
 
+    public GameObject inventory;
+
+    bool isActive;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            isActive = !isActive;
+        }
+
+        inventory.SetActive(isActive);
     }
 }
